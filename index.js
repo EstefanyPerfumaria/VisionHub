e214b1be62956ddfe60a937f263a2525118b13e7
MorcergoDls <114428118+EstefanyPerfumaria@users.noreply.github.com>
2026-03-27 03:42:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
 const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
 
-client.once('ready', () => {
+const commands = [
+  new SlashCommandBuilder()
+    .setName('say')
+    .setDescription('Faz o bot falar')
+    .addStringOption(option =>
+      option.setName('msg')
+        .setDescription('Mensagem')
+        .setRequired(true))
+].toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// REGISTRA O COMANDO
+client.once('clientReady', async () => {
   console.log(`Bot online: ${client.user.tag}`);
+
+  try {
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: commands },
+    );
+    console.log('Comando /say registrado!');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
+// EXECUTA O COMANDO
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
